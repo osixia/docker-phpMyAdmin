5fe674f6189cadfae2f605841366afb9506c3c59
Administrator <admin@example.com>
2014-08-20 13:59:17 +0200

    use new baseimage

--- service/phpmyadmin/phpmyadmin.sh
+++ service/phpmyadmin/phpmyadmin.sh
@@ -69,22 +69,20 @@ if [ ! -e /etc/phpmyadmin/docker_bootstrapped ]; then
   ln -s ../conf.d/mcrypt.so /etc/php5/mods-available/mcrypt.so
   php5enmod mcrypt
 
-  # nginx config
-  ln -s /etc/nginx/sites-available/phpmyadmin /etc/nginx/sites-enabled/phpmyadmin
-  rm /etc/nginx/sites-enabled/default
+  # nginx config (tools from osixia/baseimage)
+  /sbin/nginx-add-vhost localhost /usr/share/phpmyadmin --php --ssl --ssl-crt=/etc/nginx/ssl/$PHPMYADMIN_SSL_CRT_FILENAME --ssl-key=/etc/nginx/ssl/$PHPMYADMIN_SSL_KEY_FILENAME
+  /sbin/nginx-remove-vhost default
+
+  # php options
+  PHP_CONFIG=`cat /etc/phpmyadmin/phpmyadmin.nginx.php.config`
+  /sbin/nginx-vhost-add-php-config localhost "$PHP_CONFIG"
+
+  # nginx custom config
+  NGINX_CONFIG=`cat /etc/phpmyadmin/phpmyadmin.nginx.config`
+  /sbin/nginx-vhost-add-config localhost "$NGINX_CONFIG"
 
   ## Fix some security stuff
   ## https://wiki.phpmyadmin.net/pma/Security
-
-  # An osixia/mariadb container is linked to this phpMyAdmin container
-  if [ -n "${DB_NAME}" ]; then
-
-    # Remove root access from docker network
-    mysql -u $DB_ROOT_USER -p$DB_ROOT_PWD -h $DB_HOST -e "DELETE FROM mysql.user WHERE User='root' and Host='172.17.%.%';"
-    mysql -u $DB_ROOT_USER -p$DB_ROOT_PWD -h $DB_HOST -e "FLUSH PRIVILEGES;"
-
-  fi
-
 
   # Remove setup directory
   rm -rf /usr/share/phpmyadmin/setup
