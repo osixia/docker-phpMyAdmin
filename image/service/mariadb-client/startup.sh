612ac74711db3018e3bba91e9bfa4e4b1eda1903
Bertrand Gouny <bgouny@hipay.com>
2016-01-20 17:13:35 +0100

    web-baseimage:0.1.8

--- image/service/mariadb-client/startup.sh
+++ image/service/mariadb-client/startup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
 
-FIRST_START_DONE="/etc/docker-mariadb-client-first-start-done"
+# set -x (bash debug) if log level is trace
+# https://github.com/osixia/docker-light-baseimage/blob/stable/image/tool/log-helper
+log-helper level eq trace && set -x
 
+FIRST_START_DONE="${CONTAINER_STATE_DIR}/docker-mariadb-client-first-start-done"
 # container first start
 if [ ! -e "$FIRST_START_DONE" ]; then
 
   #
   # Search ssl config in hosts config and call cfssl-helper
   #
-
   function host_info() {
 
     local ssl_ca=""
@@ -16,12 +18,10 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     local ssl_key=""
 
     # iterate all host config
-    for info in $(complex-bash-env iterate "$2")
+    for info in $(complex-bash-env iterate "$1")
     do
 
-      local isRow=$(complex-bash-env isRow "${!info}")
-
-      if [ $isRow = true ]; then
+      if [ $(complex-bash-env isRow "${!info}") = true ]; then
         local key=$(complex-bash-env getRowKey "${!info}")
         local value=$(complex-bash-env getRowValue "${!info}")
 
@@ -36,17 +36,16 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     done
 
     if [ -n "$ssl_cert" ] && [ -n "$ssl_key" ] && [ -n "$ssl_ca" ]; then
-      cfssl-helper mariadb "$ssl_cert" "$ssl_key" "$ssl_ca"
+      cfssl-helper ${MARIADB_CLIENT_CFSSL_PREFIX} "$ssl_cert" "$ssl_key" "$ssl_ca"
     fi
 
   }
 
   # iterate all hosts
-  for host in $(complex-bash-env iterate "${PHPMYADMIN_DB_HOSTS}")
+  for host in $(complex-bash-env iterate PHPMYADMIN_DB_HOSTS)
   do
-    isRow=$(complex-bash-env isRow "${!host}")
-    if [ $isRow = true ]; then
-      info=$(complex-bash-env getRowValue "${!host}")
+    if [ $(complex-bash-env isRow "${!host}") = true ]; then
+      info=$(complex-bash-env getRowValueVarName "${!host}")
       host_info "$info"
     fi
   done
