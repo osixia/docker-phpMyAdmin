c647a9c56de09d8b43000f2fa6459ed818f35425
Bertrand Gouny <bertrand.gouny@osixia.net>
2017-07-13 12:31:02 +0200

    remove max_childrenconfig

--- image/service/phpmyadmin/install.sh
+++ image/service/phpmyadmin/install.sh
@@ -8,9 +8,6 @@ ln -sf /container/service/phpmyadmin/assets/apache2/phpmyadmin-ssl.conf /etc/apa
 cat /container/service/phpmyadmin/assets/php7.0-fpm/pool.conf >> /etc/php/7.0/fpm/pool.d/www.conf
 rm /container/service/phpmyadmin/assets/php7.0-fpm/pool.conf
 
-# increase pm.max_children
-sed -i "s/pm\.max_children.*/pm\.max_children = 50/g" /etc/php/7.0/fpm/pool.d/www.conf
-
 mkdir -p /var/www/tmp
 chown www-data:www-data /var/www/tmp
 
